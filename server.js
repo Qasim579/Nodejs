ab701b1696f91f726aa32302b5586723b2fc22fb
Qasim579 <kqasim579@gmail.com>
2021-12-13 12:57:27 +0500

    running npm scripts

--- server.js
+++ server.js
@@ -1,7 +1,27 @@
-// import http from "http";
+const fs = require("fs");
 const http = require("http");
 const server = http.createServer((req, res) => {
-  console.log(req);
+  const url = req.url;
+  const method = req.method;
+  if (url === "/") {
+    res.write("<html>");
+    res.write("<head><title>Enter Message</title></head>");
+    res.write(
+      '<body><form action = "message" method = "POST"><input  type = "text" name = "message"><button type = "submit">Send</button></form><body>'
+    );
+    res.write("</html>");
+    return res.end();
+  }
+  if (url === "/message" && method ==='POST'){
+    fs.writeFileSync('message.txt','Dummy text');
+    res.statusCode = 302;
+    res.setHeader('Location','/');
+    return res.end();
+  } 
+  res.write("<html>");
+  res.write("<head><title>Hello Qasim nodejs server<head></title>");
+  res.write("<body><h1>the server</h1><body>");
+  res.write("</html>");
 });
 server.listen(3000);
 // process.exit();
